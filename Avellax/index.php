e2261b91dea4584f8016a042084cf90602b1e6e9
AngelMendez <mendezz.angel@gmail.com>
2016-02-11 17:10:58 -0600

    Ligero Update
    Ya va quedando

--- Avellax/index.php
+++ Avellax/index.php
@@ -211,38 +211,22 @@
                         $row = mysql_fetch_assoc($result);
                         echo "<h2 class='section-heading'>" . $row['title'] . "</h2>";
                     ?>
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    <h3 class="section-subheading text-muted">Un retrato es una pintura o efigie principalmente de una persona. También se entiende por retrato la descripción de la figura o carácter, o sea, de las cualidades físicas o morales de una persona.</h3>
                 </div>
             </div>
             <div class="row">
                 <div class="col-sm-4">
                     <div class="team-member">
                         <img src="img/team/1.jpg" class="img-responsive img-circle" alt="">
-                        <h4>Kay Garland</h4>
-                        <p class="text-muted">Lead Designer</p>
-                        <ul class="list-inline social-buttons">
-                            <li><a href="#"><i class="fa fa-twitter"></i></a>
-                            </li>
-                            <li><a href="#"><i class="fa fa-facebook"></i></a>
-                            </li>
-                            <li><a href="#"><i class="fa fa-linkedin"></i></a>
-                            </li>
-                        </ul>
+                        <h4>Hannah Montana</h4>
+                        <p class="text-muted">Retrato Individual</p>
                     </div>
                 </div>
                 <div class="col-sm-4">
                     <div class="team-member">
                         <img src="img/team/2.jpg" class="img-responsive img-circle" alt="">
-                        <h4>Larry Parker</h4>
-                        <p class="text-muted">Lead Marketer</p>
-                        <ul class="list-inline social-buttons">
-                            <li><a href="#"><i class="fa fa-twitter"></i></a>
-                            </li>
-                            <li><a href="#"><i class="fa fa-facebook"></i></a>
-                            </li>
-                            <li><a href="#"><i class="fa fa-linkedin"></i></a>
-                            </li>
-                        </ul>
+                        <h4>Sofia Ortiz</h4>
+                        <p class="text-muted">Black&White</p>
                     </div>
                 </div>
                 <div class="col-sm-4">
@@ -250,20 +234,12 @@
                         <img src="img/team/3.jpg" class="img-responsive img-circle" alt="">
                         <h4>Diana Pertersen</h4>
                         <p class="text-muted">Lead Developer</p>
-                        <ul class="list-inline social-buttons">
-                            <li><a href="#"><i class="fa fa-twitter"></i></a>
-                            </li>
-                            <li><a href="#"><i class="fa fa-facebook"></i></a>
-                            </li>
-                            <li><a href="#"><i class="fa fa-linkedin"></i></a>
-                            </li>
-                        </ul>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 text-center">
-                    <p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
+                    <p class="large text-muted">Contamos con la máxima experiencia para tomar los mejores retratos de la ciudad.</p>
                 </div>
             </div>
         </div>
